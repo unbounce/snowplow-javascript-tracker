b660dd5c7d8500fb31c840ec1d758312cc8c8d2a
Paul Boocock <paul@snowplowanalytics.com>
2020-07-29 13:12:37 +0100

    [TEMP] Only test IE9

--- tests/wdio.sauce.conf.js
+++ tests/wdio.sauce.conf.js
@@ -8,36 +8,36 @@ exports.config = {
 
   maxInstances: 5,
   capabilities: [
-    {
-      browserName: 'firefox',
-      platform: 'Windows 10',
-    },
-    {
-      browserName: 'firefox',
-      platform: 'macOS 10.14',
-    },
-    {
-      browserName: 'chrome',
-      platform: 'Windows 10',
-    },
-    {
-      browserName: 'chrome',
-      platform: 'macOS 10.14',
-    },
-    {
-      browserName: 'MicrosoftEdge',
-      platform: 'Windows 10',
-    },
-    {
-      browserName: 'internet explorer',
-      platform: 'Windows 8.1',
-      version: '11',
-    },
-    {
-      browserName: 'internet explorer',
-      platform: 'Windows 8',
-      version: '10',
-    },
+    // {
+    //   browserName: 'firefox',
+    //   platform: 'Windows 10',
+    // },
+    // {
+    //   browserName: 'firefox',
+    //   platform: 'macOS 10.14',
+    // },
+    // {
+    //   browserName: 'chrome',
+    //   platform: 'Windows 10',
+    // },
+    // {
+    //   browserName: 'chrome',
+    //   platform: 'macOS 10.14',
+    // },
+    // {
+    //   browserName: 'MicrosoftEdge',
+    //   platform: 'Windows 10',
+    // },
+    // {
+    //   browserName: 'internet explorer',
+    //   platform: 'Windows 8.1',
+    //   version: '11',
+    // },
+    // {
+    //   browserName: 'internet explorer',
+    //   platform: 'Windows 8',
+    //   version: '10',
+    // },
     {
       browserName: 'internet explorer',
       platform: 'Windows 7',
@@ -46,36 +46,36 @@ exports.config = {
     /* note the change from platform to platformName
      * that goes with the selenium version switch
      */
-    {
-      browserName: 'safari',
-      browserVersion: 'latest',
-      platformName: 'macOS 10.14',
-      'sauce:options': { seleniumVersion: '3.14.0' },
-    },
-    {
-      browserName: 'safari',
-      browserVersion: '13.0',
-      platformName: 'macOS 10.13',
-      'sauce:options': { seleniumVersion: '3.14.0' },
-    },
-    {
-      browserName: 'safari',
-      browserVersion: '11.0',
-      platformName: 'macOS 10.12',
-      'sauce:options': { seleniumVersion: '3.14.0' },
-    },
-    {
-      browserName: 'safari',
-      platformName: 'macOS 10.12',
-      browserVersion: '10.1',
-      'sauce:options': { seleniumVersion: '3.14.0' },
-    },
-    // and back to platform again
-    {
-      browserName: 'safari',
-      platform: 'OS X 10.10',
-      browserVersion: '8.0',
-    },
+    // {
+    //   browserName: 'safari',
+    //   browserVersion: 'latest',
+    //   platformName: 'macOS 10.14',
+    //   'sauce:options': { seleniumVersion: '3.14.0' },
+    // },
+    // {
+    //   browserName: 'safari',
+    //   browserVersion: '13.0',
+    //   platformName: 'macOS 10.13',
+    //   'sauce:options': { seleniumVersion: '3.14.0' },
+    // },
+    // {
+    //   browserName: 'safari',
+    //   browserVersion: '11.0',
+    //   platformName: 'macOS 10.12',
+    //   'sauce:options': { seleniumVersion: '3.14.0' },
+    // },
+    // {
+    //   browserName: 'safari',
+    //   platformName: 'macOS 10.12',
+    //   browserVersion: '10.1',
+    //   'sauce:options': { seleniumVersion: '3.14.0' },
+    // },
+    // // and back to platform again
+    // {
+    //   browserName: 'safari',
+    //   platform: 'OS X 10.10',
+    //   browserVersion: '8.0',
+    // },
   ],
   services: ['static-server', 'sauce'],
 
